2f182014f9264d241f265a3a8666ae15becff7b2
jsilv-1078 <jeff.silvestro@gmail.com>
2026-08-04 13:47:02 -0400

    Shuffle full feed on each visit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { PulseFeed } from "./pulse-feed";
 import { getFeedStories } from "@/lib/db";
+import { remixFeedStories } from "@/lib/feed-order";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const stories = await getFeedStories(12);
-  return <PulseFeed initialStories={stories}/>;
+  const stories = await getFeedStories(120);
+  return <PulseFeed initialStories={remixFeedStories(stories)}/>;
 }
