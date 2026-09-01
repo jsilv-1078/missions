87e3de492b21c0ca68fd2b857e9de5bc020cbd5b
jsilv-1078 <jeff.silvestro@gmail.com>
2026-09-01 13:23:04 -0400

    Apply Pulse image sizing safeguards

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Barlow_Condensed } from "next/font/google";
 import "./globals.css";
 import "./pulse.css";
+import "./pulse-image-fix.css";
 import "./cm-nav.css";
 
 const marketHeadline = Barlow_Condensed({ subsets:["latin"],weight:"600",style:"normal",display:"swap",variable:"--font-market-headline" });
