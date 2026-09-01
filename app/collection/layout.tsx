f7adc9145b7027af5bcfbcdbf658e826d3f426ce
jsilv-1078 <jeff.silvestro@gmail.com>
2026-09-01 13:12:33 -0400

    Add Collection page metadata

---
+++ app/collection/layout.tsx
@@ -0,0 +1,3 @@
+import type {Metadata} from 'next'
+export const metadata:Metadata={title:'Collection',description:'Track the value and performance of your Card Madness collection.'}
+export default function Layout({children}:{children:React.ReactNode}){return children}
